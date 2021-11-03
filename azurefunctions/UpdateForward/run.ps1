023e1582e1354ba7fc04bfda1660deefc2c90b17
Andy Thompson <adthom@users.noreply.github.com>
2021-11-03 17:27:38 -0400

    modernize connection logic

--- azurefunctions/UpdateForward/run.ps1
+++ azurefunctions/UpdateForward/run.ps1
@@ -17,14 +17,56 @@ $Credential = [System.Management.Automation.PSCredential]::new($UserName, ($Pass
 # Add an option to remove the machine profile
 $SessionOption = New-PSSessionOption -NoMachineProfile
 
+# put the MicrosoftTeams cmdlets used by this script (aside from Connect-MicrosoftTeams) here
+# this will allow us to check to see if we need to establish a PSRemoting session or not later
+$TeamsCmdletsUsed = @(
+    'Get-CsOnlineUser'
+    'Set-CsOnlineVoicemailUserSettings'
+)
 
 # connect to Microsoft Teams PowerShell
 $SW = [Diagnostics.Stopwatch]::StartNew()
-
 do {
     try {
-        $CsSession = [Microsoft.Teams.ConfigApi.Cmdlets.TrpsSessionPsCmds]::new().NewSession($Credential, '', '', $SessionOption)
-        Import-PSSession -Session $CsSession -AllowClobber | Out-Null
+        # check if we already have a valid MicrosoftTeams connection
+        $SessionInfo = [Microsoft.Teams.ConfigApi.Cmdlets.SessionStateStore]::TryConfigApiSessionInfo
+
+        if (!$SessionInfo) {
+            # if we do not yet have the base connection, create it.
+            Connect-MicrosoftTeams -Credential $Credential -ErrorAction Stop | Out-Null
+        }
+        $ConfigApiSessionInfo = [Microsoft.Teams.ConfigApi.Cmdlets.SessionStateStore]::ConfigApiSessionInfo
+        $ConfigApiSessionInfo.PowershellSessionOption = $SessionOption
+
+        # if the cmdlets used in this script are flighted for autorest, 
+        # we do not need to establish a PSRemotingSession, and are not hindered by account limits
+        $CommandsNeedPSRemoting = $TeamsCmdletsUsed | Where-Object { 
+            $_ -notin $ConfigApiSessionInfo.SessionConfiguration.RemotingCmdletsFlightedForAutoRest }
+        if ($CommandsNeedPSRemoting.Count -eq 0) {
+            $CsSession = $true
+            break
+        }
+
+        # get a "Get" command from the MicrosoftTeams module with no required parameters that is not enabled for AutoRest
+        # we will use this command to force creation of a PSRemoting session 
+        $TestGetCommand = Get-Command -Module MicrosoftTeams -Verb Get |
+            Where-Object {
+                $_.Name -notin $ConfigApiSessionInfo.SessionConfiguration.RemotingCmdletsFlightedForAutoRest 
+            } |
+            Where-Object {
+                $RequiredParameters = $_.ParameterSets |
+                                        Where-Object { $_.IsDefault } |
+                                        ForEach-Object { $_.Parameters } |
+                                        Where-Object { $_.IsMandatory }
+                !$RequiredParameters
+            } |
+            Select-Object -First 1
+
+        # invoke our found command, this will initiate the PSRemotingSession
+        & $TestGetCommand -ErrorAction Stop | Out-Null
+
+        # if we make it here, we have a valid session, lets grab it to confirm
+        $CsSession = Get-PSSession -Name SfBPowerShellSessionViaTeamsModule_*
     }
     catch {
         if ( $_.Exception.InnerException.ErrorCode -ne 0x803381A5 ) {
