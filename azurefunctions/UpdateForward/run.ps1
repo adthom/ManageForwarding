9090df096dfb2812750f82aa88bfc46281dcb0e3
Andy Thompson <adthom@users.noreply.github.com>
2021-11-03 21:08:30 -0400

    Modernize MicrosoftTeams PowerShell Connection (#1)
    * error handling update
    
    * Add or update the App Service deployment workflow configuration from Azure Portal.
    
    * MicrosoftTeams version requirement
    
    * modernize connection logic
    
    * remove github action
    
    * exit on error
    
    * change exit code on error
    
    * debug settings

--- azurefunctions/UpdateForward/run.ps1
+++ azurefunctions/UpdateForward/run.ps1
@@ -8,27 +8,67 @@ Write-Host "PowerShell Queue Item trigger function processed a request."
 
 Import-Module -Name MicrosoftTeams
 
-$ConnectionTimeoutSeconds = 300
+$ConnectionTimeoutSeconds = 180
 $UserName = $env:TEAMS_ADMIN_USER
 $Password = $env:TEAMS_ADMIN_PASSWORD
 
 $Credential = [System.Management.Automation.PSCredential]::new($UserName, ($Password | ConvertTo-SecureString -Force -AsPlainText))
+Write-Information "Using account: $UserName"
 
 # Add an option to remove the machine profile
 $SessionOption = New-PSSessionOption -NoMachineProfile
 
+# put the MicrosoftTeams cmdlets used by this script (aside from Connect-MicrosoftTeams) here
+# this will allow us to check to see if we need to establish a PSRemoting session or not later
+$TeamsCmdletsUsed = @(
+    'Get-CsOnlineUser'
+    'Set-CsOnlineVoicemailUserSettings'
+)
 
 # connect to Microsoft Teams PowerShell
 $SW = [Diagnostics.Stopwatch]::StartNew()
-
 do {
+    # check if we already have a valid MicrosoftTeams connection
+    $SessionInfo = [Microsoft.Teams.ConfigApi.Cmdlets.SessionStateStore]::TryConfigApiSessionInfo
+
+    if (!$SessionInfo) {
+        # if we do not yet have the base connection, create it.
+        Connect-MicrosoftTeams -Credential $Credential -ErrorAction Stop -Verbose | Out-Null
+    }
+    $ConfigApiSessionInfo = [Microsoft.Teams.ConfigApi.Cmdlets.SessionStateStore]::ConfigApiSessionInfo
+    $ConfigApiSessionInfo.PowershellSessionOption = $SessionOption
+    
+    # if the cmdlets used in this script are flighted for autorest, 
+    # we do not need to establish a PSRemotingSession, and are not hindered by account limits
+    $CommandsNeedPSRemoting = $TeamsCmdletsUsed | Where-Object { 
+        $_ -notin $ConfigApiSessionInfo.SessionConfiguration.RemotingCmdletsFlightedForAutoRest }
+    if ($CommandsNeedPSRemoting.Count -eq 0) {
+        $CsSession = $true
+        break
+    }
+    
+    # get a "Get" command from the MicrosoftTeams module with no required parameters that is not enabled for AutoRest
+    # we will use this command to force creation of a PSRemoting session 
+    $TestGetCommand = Get-Command -Module MicrosoftTeams -Verb Get | Where-Object {
+                            $_.Name -notin $ConfigApiSessionInfo.SessionConfiguration.RemotingCmdletsFlightedForAutoRest 
+                        } | Where-Object {
+                            $RequiredParameters = $_.ParameterSets |
+                            Where-Object { $_.IsDefault } |
+                            ForEach-Object { $_.Parameters } |
+                            Where-Object { $_.IsMandatory }
+                            !$RequiredParameters
+                        } | Select-Object -First 1
     try {
-        $CsSession = [Microsoft.Teams.ConfigApi.Cmdlets.TrpsSessionPsCmds]::new().NewSession($Credential, '', '', $SessionOption)
-        Import-PSSession -Session $CsSession -AllowClobber | Out-Null
+        # invoke our found command, this will initiate the PSRemotingSession
+        & $TestGetCommand -ErrorAction Stop | Out-Null
+
+        # if we make it here, we have a valid session, lets grab it to confirm
+        $CsSession = Get-PSSession -Name SfBPowerShellSessionViaTeamsModule_*
     }
     catch {
         if ( $_.Exception.InnerException.ErrorCode -ne 0x803381A5 ) {
             Write-Error "Could not connect to Microsoft Teams PowerShell: $($_.Exception.Message)"
+            exit 1
         }
         else {
             Write-Warning "Maximum admin sessions in use, retrying for next $($ConnectionTimeoutSeconds - $sw.Elapsed.TotalSeconds) seconds"
@@ -40,7 +80,13 @@ $SW.Stop()
 if ($CsSession) {
     Write-Host "Session created in $($SW.Elapsed.TotalSeconds) seconds."
     # ensure user exists:
-    $UserToUpdate = Get-CsOnlineUser -Identity $jobData.Identity
+    try {
+        $UserToUpdate = Get-CsOnlineUser -Identity $jobData.Identity -ErrorAction Stop
+    }
+    catch {
+        $UserToUpdate = $null
+        $GetUserError = $_.Exception
+    }
     if ($UserToUpdate) {
         # set voicemail forwarding
         $ForwardingParams = @{
@@ -54,6 +100,9 @@ if ($CsSession) {
     }
     else {
         Write-Warning "User $($jobData.Identity) not found!"
+        if ($GetUserError) {
+            Write-Error -Exception $GetUserError
+        }
     }
     $CsSession | Remove-PSSession
 }
